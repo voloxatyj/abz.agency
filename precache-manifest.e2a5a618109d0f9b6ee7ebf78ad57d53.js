c60ad089a7f8304e5e47a604374e3d7d79fd02eb
abc <abc@example.com>
2019-09-17 11:25:56 +0300

    Updates

---
+++ precache-manifest.e2a5a618109d0f9b6ee7ebf78ad57d53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b21e949579c71e7ced3fad35a60d250",
+    "url": "/abz.agency/index.html"
+  },
+  {
+    "revision": "4084b7159aa75254ad4e",
+    "url": "/abz.agency/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "778eebbbd0897fbe7292",
+    "url": "/abz.agency/static/css/main.758c3e24.chunk.css"
+  },
+  {
+    "revision": "4084b7159aa75254ad4e",
+    "url": "/abz.agency/static/js/2.e86b3e93.chunk.js"
+  },
+  {
+    "revision": "778eebbbd0897fbe7292",
+    "url": "/abz.agency/static/js/main.6a627139.chunk.js"
+  },
+  {
+    "revision": "cff38c7267e1f9f72bf6",
+    "url": "/abz.agency/static/js/runtime~main.ac32843d.js"
+  }
+]);
