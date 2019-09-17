448ec4a0647a59aab3e8daf96a89ded3b7a4578c
abc <abc@example.com>
2019-09-17 17:52:53 +0300

    correct

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { ButtonContainer } from "./components";
-import { background } from "../../public/imgs/bg-1";
+
 class Main extends Component {
   render() {
     return (
       <div className='main d-sm-flex justify-content-center'>
-        <img src={background} className='img-fluid' alt='background' />
+        <img src='imgs/bg-1.jpg' className='img-fluid' alt='background' />
         <div className='mainText d-flex flex-column bd-highlight ml-5 p-3'>
           <div className='textMain bd-highlight'>
             <h1>Test assignment for Frontend Developer position</h1>
